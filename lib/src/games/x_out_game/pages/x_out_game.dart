a6f965e4b949c2db4a7cabc7ed44d498c2f420ef
moatazmohamed <mohamed.moataz194@gmail.com>
2024-07-21 20:40:47 +0300

    x-out

--- lib/src/games/x_out_game/pages/x_out_game.dart
+++ lib/src/games/x_out_game/pages/x_out_game.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../src_model/export_models.dart';
 
+import '../../../core/assets_images_phonetics.dart';
 import '../../../core/games_structure/basic_of_game_data.dart';
 import '../../../core/phonetics_color.dart';
 import '../../../core/theme_text.dart';
@@ -36,8 +37,8 @@ class _XOutGameScreen extends State<XOutGameScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final isInteracting =
-        context.watch<CurrentGamePhoneticsCubit>().state.stateOfAvatar;
+    final stateOfCurrentGamePhoneticsCubit =
+        context.watch<CurrentGamePhoneticsCubit>().state;
 
     final screenHeight = MediaQuery.of(context).size.height - 90.h;
     final screenWidth = MediaQuery.of(context).size.width - 120.w;
@@ -52,129 +53,169 @@ class _XOutGameScreen extends State<XOutGameScreen> {
             stateOfStringWillSay: state.gameData?.mainLetter ?? '');
       },
       builder: (context, state) {
-        return Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(right: 15.w),
-          width: screenWidth,
-          height: screenHeight,
-          padding: EdgeInsets.only(left: 10.h, right: 10.h, bottom: 10.h),
-          decoration: BoxDecoration(
-            color: AppColorPhonetics.darkBorderColor,
-            borderRadius: BorderRadius.circular(15),
-            border:
-                Border.all(color: AppColorPhonetics.darkBorderColor, width: 5),
-          ),
-          child: Column(
-            children: [
-              Text(
-                state.gameData?.mainLetter ?? '',
-                style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: AppTheme.getFontFamily5(),
-                      color: Colors.white,
-                      height: 0,
-                      letterSpacing: 0.50,
-                    ),
-                textAlign: TextAlign.center,
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(),
+            Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                child: Draggable<String>(
+                  maxSimultaneousDrags: 1,
+                  data: state.gameData?.mainLetter ?? '',
+                  childWhenDragging: SizedBox(
+                    // height: 50.h,//(MediaQuery.of(context).size.height / 2.8).h,
+                    width: 40.w,
+                  ),
+                  feedback: Image.asset(
+                    AppImagesPhonetics.outX,
+                    // height: (MediaQuery.of(context).size.height / 2.8).h,
+                    width: 40.w,
+                  ),
+                  child: ((stateOfCurrentGamePhoneticsCubit.stateOfAvatar ==
+                          BasicOfGameData.stateOfWin))
+                      ? SizedBox(
+                          width: 40.w,
+                        )
+                      : Image.asset(
+                          AppImagesPhonetics.X,
+                          width: 40.w,
+                        ),
+                )),
+            const SizedBox(),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(right: 15.w),
+              width: screenWidth,
+              height: screenHeight,
+              padding: EdgeInsets.only(left: 10.h, right: 10.h, bottom: 10.h),
+              decoration: BoxDecoration(
+                color: AppColorPhonetics.darkBorderColor,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                    color: AppColorPhonetics.darkBorderColor, width: 5),
               ),
-              5.ph,
-              if (state.gameImages?.isNotEmpty ?? false) ...{
-                Expanded(
-                  child: GridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, // Number of columns in the grid
-                      childAspectRatio: gridItemWidth /
-                          gridItemHeight, // Aspect ratio of each item
-                    ),
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      final isSelected =
-                          state.selectedItems?.contains(index) == true;
-                      final isCorrect = state.gameImages?[index].correct == 0;
-                      final bool gameHasData =
-                          state.gameImages?.isEmpty ?? false;
-                      return gameHasData
-                          ? const SizedBox()
-                          : XOutItemWidget(
-                              imageId: state.gameImages?[index].id ?? 0,
-                              imageName: state.gameImages?[index].image ?? "",
-                              isSelected: isSelected,
-                              isCorrect: isCorrect,
-                              isWrong: state.isWrong,
-                              onTap: () async {
-                                if (context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .ableButton() &&
-                                    state.currentGameIndex ==
-                                        context
+              child: Column(
+                children: [
+                  Text(
+                    state.gameData?.mainLetter ?? '',
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: AppTheme.getFontFamily5(),
+                          color: Colors.white,
+                          height: 0,
+                          letterSpacing: 0.50,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                  5.ph,
+                  if (state.gameImages?.isNotEmpty ?? false) ...{
+                    Expanded(
+                      child: GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, // Number of columns in the grid
+                          childAspectRatio: gridItemWidth /
+                              gridItemHeight, // Aspect ratio of each item
+                        ),
+                        itemCount: 4,
+                        itemBuilder: (context, index) {
+                          final isSelected =
+                              state.selectedItems?.contains(index) == true;
+                          final isCorrect =
+                              state.gameImages?[index].correct == 0;
+                          final bool gameHasData =
+                              state.gameImages?.isEmpty ?? false;
+                          return gameHasData
+                              ? const SizedBox()
+                              : XOutItemWidget(
+                                  imageId: state.gameImages?[index].id ?? 0,
+                                  imageName:
+                                      state.gameImages?[index].image ?? "",
+                                  isSelected: isSelected,
+                                  isCorrect: isCorrect,
+                                  isWrong: state.isWrong,
+                                  onDrag: (item) async {
+                                    if (context
                                             .read<CurrentGamePhoneticsCubit>()
-                                            .state
-                                            .index) {
-                                  if (state.gameImages?[index].correct == 0) {
-                                    await context
-                                        .read<XOutCubit>()
-                                        .selectItem(index);
-                                    await context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .addSuccessAnswer(
-                                            questions:
-                                                state.listGameData.length,
-                                            correctAnswers:
-                                                (state.currentGameIndex) + 1)
-                                        .whenComplete(() async {
-                                      bool isLastQuestion = context
-                                          .read<CurrentGamePhoneticsCubit>()
-                                          .checkIfIsTheLastQuestionOfGame(
-                                              queations:
-                                                  state.listGameData.length);
-                                      if (isLastQuestion) {
-                                        // Future.delayed(
-                                        //     const Duration(seconds: 2),
-                                        //     () async {
-                                        //   Navigator.of(context).pop();
-                                        // });
-                                      } else {
-                                        await context
-                                            .read<CurrentGamePhoneticsCubit>()
-                                            .updateIndexOfCurrentGame();
+                                            .ableButton() &&
+                                        state.currentGameIndex ==
+                                            context
+                                                .read<
+                                                    CurrentGamePhoneticsCubit>()
+                                                .state
+                                                .index) {
+                                      if (state.gameImages?[index].correct ==
+                                          0) {
                                         await context
                                             .read<XOutCubit>()
-                                            .updateTheCurrentGame(
-                                                index: context
-                                                    .read<
-                                                        CurrentGamePhoneticsCubit>()
-                                                    .state
-                                                    .index);
+                                            .selectItem(index);
+                                        await context
+                                            .read<CurrentGamePhoneticsCubit>()
+                                            .addSuccessAnswer(
+                                                questions:
+                                                    state.listGameData.length,
+                                                correctAnswers:
+                                                    (state.currentGameIndex) +
+                                                        1)
+                                            .whenComplete(() async {
+                                          bool isLastQuestion = context
+                                              .read<CurrentGamePhoneticsCubit>()
+                                              .checkIfIsTheLastQuestionOfGame(
+                                                  queations: state
+                                                      .listGameData.length);
+                                          if (isLastQuestion) {
+                                            // Future.delayed(
+                                            //     const Duration(seconds: 2),
+                                            //     () async {
+                                            //   Navigator.of(context).pop();
+                                            // });
+                                          } else {
+                                            await context
+                                                .read<
+                                                    CurrentGamePhoneticsCubit>()
+                                                .updateIndexOfCurrentGame();
+                                            await context
+                                                .read<XOutCubit>()
+                                                .updateTheCurrentGame(
+                                                    index: context
+                                                        .read<
+                                                            CurrentGamePhoneticsCubit>()
+                                                        .state
+                                                        .index);
+                                          }
+                                        });
+                                      } else {
+                                        await context
+                                            .read<XOutCubit>()
+                                            .addWrongAnswer(
+                                                isWrong: state
+                                                        .gameData
+                                                        ?.gameImages?[index]
+                                                        .id ??
+                                                    0);
+                                        await context
+                                            .read<CurrentGamePhoneticsCubit>()
+                                            .addWrongAnswer(
+                                                actionOfWrongAnswer: () async {
+                                          await context
+                                              .read<XOutCubit>()
+                                              .clearWrongAnswer();
+                                        });
                                       }
-                                    });
-                                  } else {
-                                    await context
-                                        .read<XOutCubit>()
-                                        .addWrongAnswer(
-                                            isWrong: state.gameData
-                                                    ?.gameImages?[index].id ??
-                                                0);
-                                    await context
-                                        .read<CurrentGamePhoneticsCubit>()
-                                        .addWrongAnswer(
-                                            actionOfWrongAnswer: () async {
-                                      await context
-                                          .read<XOutCubit>()
-                                          .clearWrongAnswer();
-                                    });
-                                  }
-                                }
-                              },
-                              word: state.gameImages?[index].word ?? '',
-                            );
-                    },
-                  ),
-                ),
-              }
-            ],
-          ),
+                                    }
+                                  },
+                                  word: state.gameImages?[index].word ?? '',
+                                );
+                        },
+                      ),
+                    ),
+                  }
+                ],
+              ),
+            ),
+          ],
         );
       },
     );
--- lib/src/games/x_out_game/widget/item_of_x_out.dart
+++ lib/src/games/x_out_game/widget/item_of_x_out.dart
@@ -1,9 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../based_of_eng_game.dart';
 import '../../../core/assets_images_phonetics.dart';
 import '../../../core/phonetics_color.dart';
+import '../../../cubit/current_game_phonetics_cubit.dart';
 
 class XOutItemWidget extends StatelessWidget {
   const XOutItemWidget({
@@ -14,7 +17,7 @@ class XOutItemWidget extends StatelessWidget {
     required this.imageId,
     this.isWrong,
     required this.word,
-    required this.onTap,
+    required this.onDrag,
   });
 
   final String imageName;
@@ -23,48 +26,50 @@ class XOutItemWidget extends StatelessWidget {
   final int? isWrong;
   final int imageId;
   final bool isCorrect;
-  final VoidCallback? onTap;
+  final Function(DragTargetDetails<String>) onDrag;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        decoration: BoxDecoration(
-          color: isSelected
-              ? (isCorrect
-                  ? AppColorPhonetics.greenColor3
-                  : AppColorPhonetics.incorrect)
-              : Colors.white,
-          border: Border.all(color: AppColorPhonetics.skyBlueColor, width: 3),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CachedNetworkImage(
-              imageUrl: imageName,
-              fit: BoxFit.fill,
-              // height: 70.h,
-              // width: 50.w,
-              errorWidget: (context, url, error) {
-                return Center(
-                  child: Text(word),
-                );
-              },
-            ),
-            if (isWrong == imageId) ...{
-              Image.asset(
-                AppImagesPhonetics.X,
-                // height: 70.h,
-                fit: BoxFit.fill,
-              )
-            }
-          ],
-        ),
-      ),
+      // onTap: onTap,
+      child: DragTarget<String>(
+          builder: (
+            BuildContext context,
+            List<dynamic> accepted,
+            List<dynamic> rejected,
+          ) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border:
+                    Border.all(color: AppColorPhonetics.skyBlueColor, width: 3),
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: imageName,
+                    // fit: BoxFit.fill,
+                    // height: 70.h,
+                    // width: 50.w,
+                    errorWidget: (context, url, error) {
+                      return Center(
+                        child: Text(word),
+                      );
+                    },
+                  ),
+                  if (isCorrect && isSelected) ...{
+                    Image.asset(
+                      AppImagesPhonetics.X,
+                      width: 40.w,
+                    )
+                  }
+                ],
+              ),
+            );
+          },
+          onAcceptWithDetails: (item) async => onDrag(item)),
     );
   }
 }
