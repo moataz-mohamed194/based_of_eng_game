0a4c3742f749d40a4796c5d0d9f6fde699c16892
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-07-21 14:36:54 +0300

    Adding the sounds of arabic losing and success

--- lib/src/core/assets_game_sound.dart
+++ lib/src/core/assets_game_sound.dart
@@ -2,7 +2,15 @@ import 'dart:math' hide log;
 
 class AppGameSound {
   static const String mainPathOfSoundBase = 'sound/base/';
+  static const String mainPathOfArabicSoundBase = 'sound/arabic_base/';
 
+  static const String l1 = '${mainPathOfArabicSoundBase}l1.mp3';
+  static const String l2 = '${mainPathOfArabicSoundBase}l2.mp3';
+  static const String l3 = '${mainPathOfArabicSoundBase}l3.mp3';
+  static const String l4 = '${mainPathOfArabicSoundBase}l4.mp3';
+  static const String w1 = '${mainPathOfArabicSoundBase}w1.mp3';
+  static const String w2 = '${mainPathOfArabicSoundBase}w2.mp3';
+  static const String w3 = '${mainPathOfArabicSoundBase}w3.mp3';
   static const String notThatOneSound =
       "${mainPathOfSoundBase}notThatOneSound.mp3";
   static const String notThatOneSound1 =
@@ -18,6 +26,11 @@ class AppGameSound {
   static const String youFoundItSound4 =
       "${mainPathOfSoundBase}youFoundItSound4.mp3";
 
+  static List ofSoundsOfCorrectAr = [
+    w1,
+    w2,
+    w3,
+  ];
   static List ofSoundsOfCorrect = [
     youFoundItSound,
     youFoundItSound1,
@@ -26,19 +39,33 @@ class AppGameSound {
     youFoundItSound4,
   ];
 
-  static getRandomSoundOfCorrect() {
+  static getRandomSoundOfCorrect({bool? isArabic}) {
     Random random = Random();
+    if (isArabic == true) {
+      int randomIndex = random.nextInt(ofSoundsOfCorrectAr.length);
+      return ofSoundsOfCorrectAr[randomIndex];
+    }
     int randomIndex = random.nextInt(ofSoundsOfCorrect.length);
     return ofSoundsOfCorrect[randomIndex];
   }
 
+  static List ofSoundsOfWrongAr = [
+    l1,
+    l2,
+    l3,
+    l4,
+  ];
   static List ofSoundsOfWrong = [
     notThatOneSound,
     notThatOneSound1,
   ];
 
-  static getRandomSoundOfWrong() {
+  static getRandomSoundOfWrong({bool? isArabic}) {
     Random random = Random();
+    if (isArabic == true) {
+      int randomIndex = random.nextInt(ofSoundsOfWrongAr.length);
+      return ofSoundsOfWrongAr[randomIndex];
+    }
     int randomIndex = random.nextInt(ofSoundsOfWrong.length);
     return ofSoundsOfWrong[randomIndex];
   }
