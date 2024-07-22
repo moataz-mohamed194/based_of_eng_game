8e9d815fcbaed97b24f40959e4453a0156679459
moatazmohamed <mohamed.moataz194@gmail.com>
2024-07-22 21:34:33 +0300

    images

--- lib/src/core/game_types/assets_images_math.dart
+++ lib/src/core/game_types/assets_images_math.dart
@@ -1,6 +1,17 @@
 abstract class AppImagesMath {
   static const String mainPathOfImages = 'assets/math/';
-  static const String mathBg = "${mainPathOfImages}math_bg.png";
-  static const String clickTheChoose = "${mainPathOfImages}click_the_choose.png";
 
+  static const String mathBg = "${mainPathOfImages}math_bg.png";
+  static const String clickTheChoose =
+      "${mainPathOfImages}click_the_choose.png";
+  static const String ball1 = "${mainPathOfImages}ball1.svg";
+  static const String ball2 = "${mainPathOfImages}ball2.svg";
+  static const String ball3 = "${mainPathOfImages}ball3.svg";
+  static const String ball4 = "${mainPathOfImages}ball4.svg";
+  static const String ball5 = "${mainPathOfImages}ball5.svg";
+  static const String ball6 = "${mainPathOfImages}ball6.svg";
+  static const String ball7 = "${mainPathOfImages}ball7.svg";
+  static const String ball8 = "${mainPathOfImages}ball8.svg";
+  static const String ball9 = "${mainPathOfImages}ball9.svg";
+  static const String circle = "${mainPathOfImages}circle.svg";
 }
