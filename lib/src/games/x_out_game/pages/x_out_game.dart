c19c9512b959733b9c60c3f0e17603ad835b0e87
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-08-07 15:20:17 +0300

    Fix x out game

--- lib/src/games/x_out_game/pages/x_out_game.dart
+++ lib/src/games/x_out_game/pages/x_out_game.dart
@@ -59,61 +59,64 @@ class _XOutGameScreen extends State<XOutGameScreen> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             const SizedBox(),
-            if (context.read<CurrentGamePhoneticsCubit>().ableButton())
-              Container(
-                  margin: const EdgeInsets.only(bottom: 15),
-                  child: Draggable<String>(
-                    maxSimultaneousDrags: 1,
-                    data: state.gameData?.mainLetter ?? '',
-                    childWhenDragging: SizedBox(
-                      // height: 50.h,//(MediaQuery.of(context).size.height / 2.8).h,
-                      width: 40.w,
-                    ),
-                    feedback: Image.asset(
-                      AppImagesPhonetics.outX,
-                      // height: (MediaQuery.of(context).size.height / 2.8).h,
-                      width: 40.w,
-                    ),
-                    child: ((stateOfCurrentGamePhoneticsCubit.stateOfAvatar ==
-                            BasicOfGameData.stateOfWin))
-                        ? SizedBox(
-                            width: 40.w,
-                          )
-                        : Image.asset(
-                            AppImagesPhonetics.X,
-                            width: 40.w,
-                          ),
-                  )),
+            Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                child: Draggable<String>(
+                  maxSimultaneousDrags: 1,
+                  data: state.gameData?.mainLetter ?? '',
+                  childWhenDragging: SizedBox(
+                    // height: 50.h,//(MediaQuery.of(context).size.height / 2.8).h,
+                    width: 40.w,
+                  ),
+                  feedback: Image.asset(
+                    AppImagesPhonetics.outX,
+                    // height: (MediaQuery.of(context).size.height / 2.8).h,
+                    width: 40.w,
+                  ),
+                  child: ((stateOfCurrentGamePhoneticsCubit.stateOfAvatar ==
+                          BasicOfGameData.stateOfWin))
+                      ? SizedBox(
+                          width: 40.w,
+                        )
+                      : Image.asset(
+                          AppImagesPhonetics.X,
+                          width: 40.w,
+                        ),
+                )),
             const SizedBox(),
             Container(
               alignment: Alignment.center,
-              margin: EdgeInsets.only(right: 15.w),
+              margin: EdgeInsets.only(
+                  right: state.isArabic == true ? 0.w : 15.w,
+                  left: state.isArabic == false ? 0.w : 15.w),
               width: screenWidth,
               height: screenHeight,
-              padding: EdgeInsets.only(left: 10.h, right: 10.h, bottom: 10.h),
               decoration: BoxDecoration(
                 color: AppColorPhonetics.darkBorderColor,
-                borderRadius: BorderRadius.circular(15),
+                borderRadius: BorderRadius.circular(12.r),
                 border: Border.all(
-                    color: AppColorPhonetics.darkBorderColor, width: 5),
+                    color: AppColorPhonetics.darkBorderColor, width: 1.w),
               ),
               child: Column(
                 children: [
-                  Text(
-                    state.gameData?.mainLetter ?? '',
-                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: AppTheme.getFontFamily5(),
-                          color: Colors.white,
-                          height: 0,
-                          letterSpacing: 0.50,
-                        ),
-                    textAlign: TextAlign.center,
+                  Flexible(
+                    flex: 1,
+                    child: FittedBox(
+                      child: Text(
+                        state.gameData?.mainLetter ?? '',
+                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: AppTheme.getFontFamily5(),
+                              color: Colors.white,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ),
-                  5.ph,
                   if (state.gameImages?.isNotEmpty ?? false) ...{
                     Expanded(
+                      flex: MediaQuery.of(context).size.height > 750 ? 7 : 4,
                       child: GridView.builder(
                         physics: const NeverScrollableScrollPhysics(),
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -132,6 +135,7 @@ class _XOutGameScreen extends State<XOutGameScreen> {
                           return gameHasData
                               ? const SizedBox()
                               : XOutItemWidget(
+                                  index: index,
                                   imageId: state.gameImages?[index].id ?? 0,
                                   imageName:
                                       state.gameImages?[index].image ?? "",
