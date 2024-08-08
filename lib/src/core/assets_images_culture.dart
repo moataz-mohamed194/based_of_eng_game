abadeff28a30ca581576e9192af6c20e7bd3d9b5
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-08-08 11:26:25 +0300

    Adding the assets of culture

---
+++ lib/src/core/assets_images_culture.dart
@@ -0,0 +1,5 @@
+abstract class AppImagesCulture {
+  static const String mainPathOfImages = 'assets/culture/';
+  static const String clickToChoose = '${mainPathOfImages}click_the_choose.png';
+  static const String background = '${mainPathOfImages}culture_background.png';
+}
